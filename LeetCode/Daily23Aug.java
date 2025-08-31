353dc82c54074232e4315f69dcf8c80577c1faa3
Dev Tayal <devtayal04@gmail.com>
2025-08-31 16:26:39 +0530

    add

--- LeetCode/Daily23Aug.java
+++ LeetCode/Daily23Aug.java
@@ -55,6 +55,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 rotated[j][n - 1 - i] = A[i][j];
+                i++;
             }
         }
         return rotated;
